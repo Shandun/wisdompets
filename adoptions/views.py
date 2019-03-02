5d0532ab3c38c5d563b449d6845790be90ad710c
dunshan <gz2008777@gmail.com>
2019-03-01 22:07:54 -0800

    adding files

---
+++ adoptions/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from django.http import Http404
+# Create your views here.
+
+from .models import Pet
+
+
+def home(request):
+    pets = Pet.objects.all()
+    # return HttpResponse('<p>home view</p>')
+    return render(request, 'home.html', {'pets': pets})
+
+
+def pet_detail(request, id):
+    # return HttpResponse('<p>pet_detail view with the id {}</p>'.format(id))
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(request, 'pet_detail.html', {'pet': pet})
